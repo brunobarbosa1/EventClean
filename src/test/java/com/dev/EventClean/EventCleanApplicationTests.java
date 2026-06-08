9fc24f41aeb4395f40e208ddbb264a55f192bb0d
brunobarbosa1 <br.barbosa.souza1@gmail.com>
2026-06-07 23:15:04 -0300

    first commit

---
+++ src/test/java/com/dev/EventClean/EventCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.EventClean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventCleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
